2ada02b4596c1691f5039d3d473a83b0356226c3
Jagestah <ickshnab@gmail.com>
2018-06-19 09:44:16 -0600

    Updated variable to be more pythonic

--- configchecker.py
+++ configchecker.py
@@ -6,86 +6,86 @@ import time
 import re
 
 #Gather details about the device to connect to
-deviceIp = raw_input("Enter device IP: ")
-userName = raw_input("Enter username: ")
-passWord = getpass.getpass("Enter password: ")
-#unmsKey = raw_input("Enter entire UNMS Key: ")
+device_ip = raw_input("Enter device IP: ")
+un = raw_input("Enter username: ")
+pw = getpass.getpass("Enter password: ")
+#unmskey = raw_input("Enter entire UNMS Key: ")
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh.connect(hostname=deviceIp, username=userName, password=passWord)
+ssh.connect(hostname=device_ip, username=un, password=pw)
 
 conn = ssh.invoke_shell()
-conn.send('enable\n'+passWord+'\n')
+conn.send('enable\n'+pw+'\n')
 conn.send('terminal length 0\n')		#Sets unlimited terminal length so the entire of the running config is sent without further input
 time.sleep(1)
 conn.send('show run\n')
 time.sleep(1)
 conn.send('no terminal length')			#Resets terminal length to the default value
-runConfig = conn.recv(1000000)			#Saves the running config as the variable runConfig
+run_config = conn.recv(1000000)			#Saves the running config as the variable run_config
 #print(output)
 ssh.close()
 
-with open(deviceIp+'-backup.txt', 'w+') as backup:
-	backup.write(runConfig)
+with open(device_ip+'-backup.txt', 'w+') as backup:
+	backup.write(run_config)
 
 #Return the number of interfaces on the switch.
-intCount = str(runConfig.count('interface'))
-intsresult = "The switch has "+intCount+" interfaces"
-print(intsresult)
+int_count = str(run_config.count('interface'))
+int_result = "The switch has "+int_count+" interfaces"
+print(int_result)
 
 #Return the VLAN database.
-if 'vlan database' in runConfig:
-	splitConfig = runConfig.split('vlan database\r\nvlan ')
-	splitConfig2 = splitConfig[1].split('\r\n', 2)
-	vlansresult = "The following VLANs are configured on the switch: "+splitConfig2[0]
+if 'vlan database' in run_config:
+	split_config = run_config.split('vlan database\r\nvlan ')
+	split_config2 = split_config[1].split('\r\n', 2)
+	vlansresult = "The following VLANs are configured on the switch: "+split_config2[0]
 	print(vlansresult)
 else:
 	vlansresult = 'Unable to find VLAN Database.'
 	print(vlansresult)
 
 #Return the Management VLAN.
-if 'mgmt_vlan' in runConfig:
-	splitConfig = runConfig.split('mgmt_vlan ')
-	splitConfig2 = splitConfig[1].split('\r\n', 1)
-	mgmtresult = "Management VLAN - "+splitConfig2[0]
+if 'mgmt_vlan' in run_config:
+	split_config = run_config.split('mgmt_vlan ')
+	split_config2 = split_config[1].split('\r\n', 1)
+	mgmtresult = "Management VLAN - "+split_config2[0]
 	print(mgmtresult)
 else:
 	mgmtresult = 'mgmt-vlan not found'
 	print(mgmtresult)
 
 #Check for Voice VLAN - Currently not working
-#if 'voice vlan\r\n' in runConfig:
-#	splitConfig = runConfig.split('voice vlan ')
-#	splitConfig2 = splitConfig[1].split('\r\n', 1)
-#	print("Voice VLAN - "+splitConfig2[0])
+#if 'voice vlan\r\n' in run_config:
+#	split_config = run_config.split('voice vlan ')
+#	split_config2 = split_config[1].split('\r\n', 1)
+#	print("Voice VLAN - "+split_config2[0])
 #else:
 #	print('Voice VLAN not found')
 
 #List number of Users
-usersCount = str(runConfig.count('username'))
-usersresult = 'The switch has '+usersCount+' user(s)'
-print(usersresult)
-for item in runConfig.split("\n"):
+user_list = str(run_config.count('username'))
+user_result = 'The switch has '+user_list+' user(s)'
+print(user_result)
+for item in run_config.split("\n"):
 	if "username" in item:
 		item = item.split('"')
 		print("	"+item[1])
 
-#Compare unmsKey against the UNMS key found in the config - not complete
-#if 'unms key ' in runConfig:
-#	splitConfig = runConfig.split('unms key ')
-#	splitConfig2 = splitConfig[1].split('\r\n', 1)
-#	mgmtresult = "Management VLAN - "+splitConfig2[0]
+#Compare unmskey against the UNMS key found in the config - not complete
+#if 'unms key ' in run_config:
+#	split_config = run_config.split('unms key ')
+#	split_config2 = split_config[1].split('\r\n', 1)
+#	mgmtresult = "Management VLAN - "+split_config2[0]
 #	print(mgmtresult)
 #else:
 #	mgmtresult = 'mgmt-vlan not found'
 #	print(mgmtresult)
 
 #Count DNS name-servers
-dnsresult = "The switch's name servers"
-print(dnsresult)
-for item in runConfig.split("\n"):
+dns_result = "The switch's name servers"
+print(dns_result)
+for item in run_config.split("\n"):
 	if "name server" in item:
 		item = item.split('ip name server ')
 		print("	"+item[1])
